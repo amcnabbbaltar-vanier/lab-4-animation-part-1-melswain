aaa929a747ed6b517120ec2602ce5411595b8c48
Melanie Swain <6228040@edu.vaniercollege.qc.ca>
2025-09-17 12:23:34 -0400

    Completed lab

---
+++ Assets/Character/PlayerAnimatiorController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatiorController : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterMovement movement;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        movement = GetComponent<CharacterMovement>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("CharacterSpeed", rb.velocity.magnitude);
+        animator.SetBool("IsGrounded", movement.IsGrounded);
+       
+        if (Input.GetButtonUp("Fire1"))
+        {
+            animator.SetTrigger("doRoll");
+        }
+    }
+}
